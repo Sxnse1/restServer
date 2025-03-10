5665b985e041597e0f9f38fec262f79a69cc9658
César Alejandro Dávila Reyna <126361906+Sxnse1@users.noreply.github.com>
2025-03-10 12:17:59 -0600

    final

--- models/server.js
+++ models/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 class Server {
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
 
         //Middlewares
         this.middlewares();
@@ -18,12 +20,18 @@ class Server {
         //Directorio publico
         this.app.use(express.static('public'));
 
-    }
+        //Cors
+        this.app.use(cors());
+
+        //Lectura y parseo del body
+        this.app.use(express.json()); 
+
+    };
 
     routes() {
-        this.app.get('/api', (req, res) => {
-            res.send('Hello World')
-        });
+        
+        this.app.use( this.usuariosPath, require('../routes/usuarios'));
+
     };
 
     listen() {
